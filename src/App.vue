11bda2436eb0985f246946b7ae21d58b32487cc1
ajiprasetyo471 <ajiprsty4713@gmail.com>
2023-02-22 20:51:37 +0700

    slicing and integration

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <ProfilePicGenerator />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ProfilePicGenerator from './components/ProfilePicGenerator.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: { ProfilePicGenerator },
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  padding: 0;
+  margin: 0;
+}
+
+.container {
+  width: 100%;
+  height: 100vh;
+  background: #fff377;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
---
+++ src/components/ProfilePicGenerator.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="card-container">
+    <input
+      type="text"
+      class="input-name"
+      v-model="name"
+      placeholder="Ketik nama Anda..."
+      v-on:keyup.enter="getProfilePicture"
+      onke
+    />
+    <button @click="getProfilePicture" class="create-pic">
+      Buat Profil Picture!
+    </button>
+    <img :src="profilePic" class="profile-pic" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProfilePicGenerator',
+  data() {
+    return {
+      name: '',
+      profilePic: '',
+    };
+  },
+  methods: {
+    getProfilePicture() {
+      this.profilePic = `https://robohash.org/${this.name || ''}`;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.card-container {
+  width: 50vw;
+  height: 80vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 15px;
+  background: #e0ffff;
+
+  .input-name {
+    margin-top: 30px;
+    width: 20vw;
+    padding: 10px;
+    font-size: 14px;
+    border-radius: 5px;
+    border: 1.5px solid #d2dfe3;
+    color: #ac9e8f;
+  }
+
+  .input-name:focus {
+    outline: none;
+  }
+
+  .create-pic {
+    background: #28a745;
+    color: #fff;
+    border: none;
+    padding: 10px;
+    font-weight: 500;
+    font-size: 14px;
+    border-radius: 5px;
+    width: 10vw;
+  }
+
+  .create-pic:active {
+    outline: 3px solid #84d3a2;
+  }
+
+  .profile-pic {
+    margin-top: 10px;
+    width: 20vw;
+    background: #fff;
+    border-radius: 50%;
+    border: 2px solid #eee;
+  }
+}
+</style>
